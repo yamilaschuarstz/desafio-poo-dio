bbbfd303174b88b18b4ff73eb8d40ff1fe6037ef
yams <yamschuartz@gmail.com>
2022-08-05 11:41:33 -0300

    Mentoria e Curso

---
+++ src/br/com/dio/dominio/Main.java
@@ -0,0 +1,29 @@
+package br.com.dio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição curso JS");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição de mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
+}
